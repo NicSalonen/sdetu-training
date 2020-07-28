1e16122467b46cc97593f49b09089fdde105ca5c
NicSalonen <68102217+NicSalonen@users.noreply.github.com>
2020-07-28 20:29:23 +0200

    Update HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
  	public static void main(String[] args) {
 			System.out.println("Hello world");
 			System.out.println("Some new code has been added");
+			System.out.println("Adding code from GitHub");
 		}
 }
